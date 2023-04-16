51bf567592912b2771ad499db14499c4a6be0eca
Atharv Chandratre <atharvchandratre@gmail.com>
2023-04-15 21:00:08 -0500

    Revert "Modified create_lfs.py"
    This reverts commit 13ec8385190a0ce6b286bdf16cc0c3c4613a0185.

--- keyclass/create_lfs.py
+++ keyclass/create_lfs.py
@@ -1,88 +1,120 @@
+# coding=utf-8
+# MIT License
+
+# Copyright (c) 2020 Carnegie Mellon University, Auton Lab
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import nltk
-from sklearn.semi_supervised import LabelPropagation
-import torch
-import models
+
 nltk.download('stopwords')
 from nltk import download, pos_tag, corpus
-from scipy.spatial import distance
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import numpy as np
 import pandas as pd
+from scipy.spatial import distance
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+# from sklearn.semi_supervised import LabelPropagation
+import torch
+import models
 
 
 def get_vocabulary(text_corpus, max_df=1.0, min_df=0.01, ngram_range=(1, 1)):
+    """Returns vocabulary and word indicator matrix
+    The word indicator matrix is a n x m matrix corresponding to n documents and m words in the
+    vocabulary. 
 
-    strip_accents = 'unicode'
-    stopwords = corpus.stopwords.words('english')
-    vectorizer = CountVectorizer()
-    vectorizer.max_df = max_df
-    vectorizer.min_df = min_df
-    vectorizer.strip_accents = strip_accents
-    vectorizer.stop_words = stopwords
-    vectorizer.ngram_range = ngram_range
+    """
+    # Vectorizing the vocabulary
+    vectorizer = CountVectorizer(max_df=max_df,
+                                 min_df=min_df,
+                                 strip_accents='unicode',
+                                 stop_words=corpus.stopwords.words('english'),
+                                 ngram_range=ngram_range)
 
-    transformed = vectorizer.fit_transform(text_corpus)
-    word_indicator_matrix = transformed.toarray()
-
-    feature_names_out = vectorizer.get_feature_names_out()
-    vocabulary = np.asarray(feature_names_out)
+    word_indicator_matrix = vectorizer.fit_transform(text_corpus).toarray()
+    vocabulary = np.asarray(vectorizer.get_feature_names_out())  # Vocabulary
 
     return word_indicator_matrix, vocabulary
 
 
-def assign_categories_to_keywords(vocabulary, vocabulary_embeddings, label_embeddings, word_indicator_matrix, cutoff=None, topk=None, min_topk=True):
+def assign_categories_to_keywords(vocabulary,
+                                  vocabulary_embeddings,
+                                  label_embeddings,
+                                  word_indicator_matrix,
+                                  cutoff=None,
+                                  topk=None,
+                                  min_topk=True):
 
-    cutoffIsNone = cutoff is None
-    topkIsNone = topk is None
-    assert (cutoffIsNone or topkIsNone)
+    assert ((cutoff is None) or (topk is None))
 
-    metric = 'cosine'
+    distances = distance.cdist(vocabulary_embeddings, label_embeddings,
+                               'cosine')
 
-    distances = distance.cdist(vocabulary_embeddings, label_embeddings, metric)
-    axis = 1
-    dist_to_closest_cat = np.min(distances, axis=axis)
-    assigned_category = np.argmin(distances, axis=axis)
+    dist_to_closest_cat = np.min(distances, axis=1)
+    assigned_category = np.argmin(distances, axis=1)
 
     if cutoff is not None:
-        mask = (dist_to_closest_cat <= cutoff)
-        mask = mask.astype(bool)
+        # make mask based off of similarity score and cutoff
+        mask = (dist_to_closest_cat <= cutoff).astype(bool)
 
     if topk is not None:
+        # make mask based off of topk closest assigned category
+        # WARNING could result on one class taking keywords from another class
         uniques = np.unique(assigned_category)
-        len_dist_closest_to_cat = len(dist_to_closest_cat)
-        mask = np.zeros(len_dist_closest_to_cat, dtype=bool)
+        mask = np.zeros(len(dist_to_closest_cat), dtype=bool)
 
-        counts = np.unique(assigned_category, return_counts=True)[1]
+        _, counts = np.unique(assigned_category, return_counts=True)
         print('Found assigned category counts', counts)
-        topk = np.min([topk, np.min(counts)]) if min_topk is True else topk
+        if min_topk == True:
+            topk = np.min([topk, np.min(counts)])
 
         for u in uniques:
-            u_inds,  = np.where(assigned_category == u)
+            u_inds = np.where(assigned_category == u)[0]
             u_dists = dist_to_closest_cat[u_inds]
-            all_sorted_inds = np.argsort(u_dists)
-            sorted_inds = all_sorted_inds[:topk]
+            sorted_inds = np.argsort(u_dists)[:topk]
             mask[u_inds[sorted_inds]] = 1
 
     keywords = vocabulary[mask]
     assigned_category = assigned_category[mask]
-    maskForWordIndicatorMatrix = np.where(mask)[0]
-    word_indicator_matrix = word_indicator_matrix[:, maskForWordIndicatorMatrix]
+    word_indicator_matrix = word_indicator_matrix[:, np.where(mask)[0]]
     return keywords, assigned_category, word_indicator_matrix
+
 
 def create_label_matrix(word_indicator_matrix, keywords, assigned_category):
 
     word_indicator_matrix = np.where(word_indicator_matrix == 0, -1, 0)
-    len_assigned_category = len(assigned_category)
-    for i in range(len_assigned_category):
-        range_word_indicator_matrix = word_indicator_matrix[:, i]
-        ith_assigned_category = assigned_category[i]
-        word_indicator_matrix[:,i] = np.where(range_word_indicator_matrix != -1, ith_assigned_category, -1)
+    for i in range(len(assigned_category)):
+        word_indicator_matrix[:,
+                              i] = np.where(word_indicator_matrix[:, i] != -1,
+                                            assigned_category[i], -1)
 
     return pd.DataFrame(word_indicator_matrix, columns=keywords)
 
 
 class CreateLabellingFunctions:
-    def __init__(self, base_encoder='paraphrase-mpnet-base-v2', device: torch.device = torch.device("cuda"), label_model: str = 'data_programming'):
+    """Class to create and store labelling functions.             
+    """
+
+    def __init__(self,
+                 base_encoder='paraphrase-mpnet-base-v2',
+                 device: torch.device = torch.device("cuda"),
+                 label_model: str = 'data_programming'):
 
         self.device = device
         self.encoder = models.Encoder(model_name=base_encoder, device=device)
@@ -106,58 +138,60 @@ class CreateLabellingFunctions:
                    label_model_n_epochs,
                    verbose=True,
                    n_classes=2):
+        ## main driver function
 
+        ## get the bert embeddings of the categories
         self.label_embeddings = self.encoder.encode(sentences=label_names)
 
         ## get vocab according to n-grams
-        self.matrix_and_vocabulary = get_vocabulary(\
+        self.word_indicator_matrix, self.vocabulary = get_vocabulary(\
             text_corpus=text_corpus,
             max_df=1.0,
             min_df=min_df,
             ngram_range=ngram_range)
-        self.word_indicator_matrix = self.matrix_and_vocabulary[0]
-        self.vocabulary = self.matrix_and_vocabulary[1]
 
-        self.vocabulary_embeddings = self.encoder.encode(sentences=self.vocabulary)
+        # embed vocab to compare with label_embeddings
+        self.vocabulary_embeddings = self.encoder.encode(
+            sentences=self.vocabulary)
 
-        self.assigned_categories_to_keywords = assign_categories_to_keywords(\
+        # labeler.assign_categories_to_keywords(cutoff=0.9)
+        self.keywords, self.assigned_category, self.word_indicator_matrix = assign_categories_to_keywords(\
             vocabulary=self.vocabulary,
             vocabulary_embeddings=self.vocabulary_embeddings,
             label_embeddings=self.label_embeddings,
             word_indicator_matrix=self.word_indicator_matrix,
             topk=topk)
-        self.keywords = self.assigned_categories_to_keywords[0]
-        self.assigned_category = self.assigned_categories_to_keywords[1]
-        self.word_indicator_matrix = self.assigned_categories_to_keywords[2]
 
         if verbose:
-            shape = self.word_indicator_matrix.shape
-            vocab = self.vocabulary
-            print('labeler.vocabulary:\n', len(vocab))
-            print('labeler.word_indicator_matrix.shape', shape)
+            print('labeler.vocabulary:\n', len(self.vocabulary))
+            print('labeler.word_indicator_matrix.shape',
+                  self.word_indicator_matrix.shape)
 
-            keywords = self.keywords
-            print('Len keywords', len(keywords))
-
-            assigned_category = self.assigned_category
+            print('Len keywords', len(self.keywords))
             print('assigned_category: Unique and Counts',
-                  np.unique(assigned_category, return_counts=True))
-
+                  np.unique(self.assigned_category, return_counts=True))
             for u in range(len(label_names)):
                 inds = np.where(self.assigned_category == u)[0]
-                inds_keywords = self.keywords[inds]
-                u_label = label_names[u]
-                print(u_label, inds_keywords)
+                print(label_names[u], self.keywords[inds])
 
-        return models.LabelModelWrapper(\
-            label_matrix=create_label_matrix(\
+        self.label_matrix = create_label_matrix(\
             word_indicator_matrix=self.word_indicator_matrix,
             keywords=self.keywords,
-            assigned_category=self.assigned_category),
+            assigned_category=self.assigned_category)
+
+        #     print('labeler.label_matrix', np.unique(labeler.label_matrix, return_counts=True))
+        label_model = models.LabelModelWrapper(\
+            label_matrix=self.label_matrix,
             n_classes=n_classes,
             y_train=y_train,
             device=self.device,
-            model_name=self.label_model_name).train_label_model(\
+            model_name=self.label_model_name)
+
+        label_model.train_label_model(\
             lr=label_model_lr,
             n_epochs=label_model_n_epochs,
-            cuda=True if torch.cuda.is_available() else False).predict_proba().values
+            cuda=True if torch.cuda.is_available() else False)
+
+        proba_preds = label_model.predict_proba().values
+
+        return proba_preds
