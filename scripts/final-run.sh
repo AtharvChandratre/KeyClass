1fe22f30b7aac3253a0643e3479d7dcfdd2d2981
Atharv Chandratre <atharvchandratre@gmail.com>
2023-04-15 22:06:21 -0500

    Changed config_mimic.yaml stats

--- scripts/final-run.sh
+++ scripts/final-run.sh
@@ -7,8 +7,8 @@ cd KeyClass/scripts/
 
 mkdir data/
 cd data/
-#FILE_ID="1tFnqlu7MvHOrfUM6jK7oPQ7-3oSQr2Z_"
-FILE_ID="1MtCNwwhq0D9N5TdM2pZliukyOzXz5bbd"
+FILE_ID="1tFnqlu7MvHOrfUM6jK7oPQ7-3oSQr2Z_"
+#FILE_ID="1MtCNwwhq0D9N5TdM2pZliukyOzXz5bbd"
 URL="https://docs.google.com/uc?export=download&id=$FILE_ID"
 echo ${green}===Downloading MIMIC Data...===${reset}
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate $URL -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILE_ID" -O "mimic.zip" && rm -rf /tmp/cookies.txt
